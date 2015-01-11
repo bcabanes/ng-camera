d59919006b4f38bbef0f222bbf715d547a794c4e
bcabanes <contact@benjamincabanes.com>
2015-01-11 14:24:48 -0500

    default example

---
+++ javascripts/angular/controller.js
@@ -0,0 +1,17 @@
+(function(angular) {
+    'use strict';
+
+    angular
+    .module('camera')
+    .controller('cameraController', controller);
+
+    controller.$inject = [];
+
+    function controller() {
+        /* jshint validthis: true */
+        var vm = this;
+
+        vm.picture = false; // Initial state
+    }
+
+})(angular);
