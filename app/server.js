b8969866758e10972e92a71663d52845d4b9dfe8
bcabanes <contact@benjamincabanes.com>
2015-01-11 12:56:26 -0500

    move file for examples

--- app/server.js
+++ app/server.js
@@ -1,3 +1,3 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/app')).listen(3000);
+connect().use(serveStatic(__dirname)).listen(3000);
